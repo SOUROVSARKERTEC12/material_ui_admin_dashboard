ebca4580b75bb5edc7a281ab55feae008e3db4b0
Admin_laravel <sourovsarker005@gmail.com>
2023-09-26 02:15:40 +0600

    Calendar work start

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Team from "./scenes/team/Team.jsx";
 import Contacts from "./scenes/contacts/Contacts.jsx"
 import Invoices from "./scenes/invoices/Invoices.jsx";
 import Form from "./scenes/form/Form.jsx";
+import Calendar from "./scenes/calendar/Calendar.jsx";
 
 function App() {
     const [theme, colorMode] = useMode()
@@ -25,6 +26,7 @@ function App() {
                             <Route path="/contacts" element={<Contacts />} />
                             <Route path="/invoices" element={<Invoices />} />
                             <Route path="/form" element={<Form />} />
+                            <Route path="/calendar" element={<Calendar />} />
                         </Routes>
                     </main>
                 </div>
---
+++ src/scenes/calendar/Calendar.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+// import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+// import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import {
+    Box,
+    List,
+    ListItem,
+    ListItemText,
+    Typography,
+    useTheme,
+} from "@mui/material";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+
+const Calendar = ()=>{
+    const theme = useTheme()
+    const colors = tokens(theme.palette.mode)
+    const [currentEvent, setCurrentEvent] = useState([])
+
+    const handleDateClick = (selected)=>{
+        const title = prompt("Please enter a new title for your event")
+        const calendarApi = selected.view.calendar;
+        calendarApi.unselect();
+
+        if(title){
+            calendarApi.addEvent({
+                id:`${selected.dateStr}-${title}`,
+                title,
+                start: selected.startStr,
+                end: selected.endStr,
+                allDay: selected.allDay,
+            })
+        }
+    }
+
+    return (
+        <Box m="20px">
+            <Header title="Calendar" subtitle="Full Calendar Interactivie Page"/>
+        </Box>
+    )
+}
+
+export default Calendar
