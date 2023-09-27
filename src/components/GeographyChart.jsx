b1c1c71d848c5e3e21ab5ff5785683ea6ab5ec12
Admin_laravel <sourovsarker005@gmail.com>
2023-09-28 05:01:55 +0600

    GeographyChart add in Geography update

--- src/components/GeographyChart.jsx
+++ src/components/GeographyChart.jsx
@@ -5,9 +5,9 @@ import { tokens } from "../theme";
 import { mockGeographyData as data } from "../data/mockData";
 
 // eslint-disable-next-line react/prop-types
-const GeographyChart = ({isDashboard = false})=>{
-    const theme = useTheme()
-    const colors = tokens(theme.palette.mode)
+const GeographyChart = ({ isDashboard = false }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
     return (
         <ResponsiveChoropleth
             data={data}
@@ -41,45 +41,46 @@ const GeographyChart = ({isDashboard = false})=>{
             }}
             features={geoFeatures.features}
             margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-            colors="nivo"
-            domain={[ 0, 1000000 ]}
+            domain={[0, 1000000]}
             unknownColor="#666666"
             label="properties.name"
             valueFormat=".2s"
             projectionScale={isDashboard ? 40 : 150}
             projectionTranslation={isDashboard ? [0.49, 0.6] : [0.5, 0.5]}
-            projectionRotation={[ 0, 0, 0 ]}
-            enableGraticule={true}
-            graticuleLineColor="#dddddd"
+            projectionRotation={[0, 0, 0]}
             borderWidth={1.5}
             borderColor="#ffffff"
-            legends={!isDashboard ?[
-                {
-                    anchor: 'bottom-left',
-                    direction: 'column',
-                    justify: true,
-                    translateX: 20,
-                    translateY: -100,
-                    itemsSpacing: 0,
-                    itemWidth: 94,
-                    itemHeight: 18,
-                    itemDirection: 'left-to-right',
-                    itemTextColor: colors.grey[100],
-                    itemOpacity: 0.85,
-                    symbolSize: 18,
-                    effects: [
+            legends={
+                !isDashboard
+                    ? [
                         {
-                            on: 'hover',
-                            style: {
-                                itemTextColor: '#ffffff',
-                                itemOpacity: 1
-                            }
-                        }
+                            anchor: "bottom-left",
+                            direction: "column",
+                            justify: true,
+                            translateX: 20,
+                            translateY: -100,
+                            itemsSpacing: 0,
+                            itemWidth: 94,
+                            itemHeight: 18,
+                            itemDirection: "left-to-right",
+                            itemTextColor: colors.grey[100],
+                            itemOpacity: 0.85,
+                            symbolSize: 18,
+                            effects: [
+                                {
+                                    on: "hover",
+                                    style: {
+                                        itemTextColor: "#ffffff",
+                                        itemOpacity: 1,
+                                    },
+                                },
+                            ],
+                        },
                     ]
-                }
-            ] : undefined}
+                    : undefined
+            }
         />
-    )
-}
+    );
+};
 
-export default GeographyChart
+export default GeographyChart;
